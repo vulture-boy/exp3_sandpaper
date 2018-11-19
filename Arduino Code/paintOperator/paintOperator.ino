93e915ba443ba4c9ab1dc76b051f05b0be9cf7d2
npyalex <np.alexander@gmail.com>
2018-11-19 13:43:36 -0500

    Solenoids

--- Arduino Code/paintOperator/paintOperator.ino
+++ Arduino Code/paintOperator/paintOperator.ino
@@ -9,17 +9,19 @@
 //*****BE SURE TO INSTALL VERSION 5.13.3 OF THE LIBRARY IT INSTALL V6BETA BY DEFAULT 
 
 
-int LEDpin1 = 11;
-int LEDpin2 = 10;
+const int paintPin1 = 4;                         //paintPints are digital pins
+const int paintPin2 = 5;
 
 int p5Input1;                                   //these variables hold the input values
 int p5Input2;
 
+int lastRead;
+
 void setup() 
 {
   Serial.begin(9600);                                     //turn on the serial port
-  pinMode(LEDpin1,OUTPUT);                       //  
-  pinMode(LEDpin2,OUTPUT);
+  pinMode(paintPin1,OUTPUT);                       // paintPins are digital outputs. 
+  pinMode(paintPin2,OUTPUT);
 }
 
 void loop() 
@@ -29,10 +31,24 @@ void loop()
   DynamicJsonBuffer messageBuffer(200);                   //create the Buffer for the JSON object        
   JsonObject& p5Read = messageBuffer.parse(Serial);      //create a JsonObject variable and attach it to incoming Serial messages     
  
-  p5Input1 = p5Read["somethingsomething"];
-  p5Input2 = p5Read["led2"];
+  p5Input1 = p5Read["somethingsomething"];              //blue user on the network clicked
+  p5Input2 = p5Read["led2"];                            //red user on the network clicked
 
-  // Operation
-  analogWrite(LEDpin1,p5Input1);
-  analogWrite(LEDpin2,p5Input2); 
+  // Paint Bucket 1
+    if(p5Input1=="makePaintGo"){                        //close the valve after half a second
+      analogWrite(paintPin1, HIGH);//copypaste this function and change the numbering to add new paint buckets
+    }else{
+      if(millis()-lastRead>=500){
+        analogWrite(paintPin1, LOW); 
+      }
+    }
+  // Paint Bucket 2
+    if(p5Input2=="makePaintGo"){                        //close the valve after half a second
+      analogWrite(paintPin2, HIGH);
+    }else{
+      if(millis()-lastRead>=500){
+        analogWrite(paintPin2, LOW); 
+      }
+    }    
+ lastRead = millis();
 }
