838e0d56152da68d99db6424cf2f213b932040c0
Tyson Moll (vultures) <tyson.moll@mail.utoronto.ca>
2018-11-19 14:00:19 -0500

    Minor code cleanup

--- Arduino Code/paintOperator/paintOperator.ino
+++ Arduino Code/paintOperator/paintOperator.ino
@@ -11,6 +11,7 @@
 
 const int paintPin1 = 4;                         //paintPints are digital pins
 const int paintPin2 = 5;
+const int paintDelay = 500; // ms, how long to maintain a high signal after receiving one 
 
 int p5Input1;                                   //these variables hold the input values
 int p5Input2;
@@ -35,20 +36,22 @@ void loop()
   p5Input2 = p5Read["led2"];                            //red user on the network clicked
 
   // Paint Bucket 1
-    if(p5Input1=="makePaintGo"){                        //close the valve after half a second
-      analogWrite(paintPin1, HIGH);//copypaste this function and change the numbering to add new paint buckets
-    }else{
-      if(millis()-lastRead>=500){
+  if (p5Input1 == "makePaintGo") {                        //close the valve after half a second
+    analogWrite(paintPin1, HIGH);//copypaste this function and change the numbering to add new paint buckets
+  } else {
+    if (millis() - lastRead >= 500) {
         analogWrite(paintPin1, LOW); 
-      }
     }
+  }
+  
   // Paint Bucket 2
-    if(p5Input2=="makePaintGo"){                        //close the valve after half a second
+  if (p5Input2=="makePaintGo") {                        //close the valve after half a second
       analogWrite(paintPin2, HIGH);
-    }else{
-      if(millis()-lastRead>=500){
-        analogWrite(paintPin2, LOW); 
-      }
-    }    
+  } else {
+    if (millis() - lastRead >= 500) {
+      analogWrite(paintPin2, LOW); 
+    }
+  }    
+  
  lastRead = millis();
 }
